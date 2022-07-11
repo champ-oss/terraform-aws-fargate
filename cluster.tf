82036077324d003d0687025e12bcf1b3e1ca3621
Jeremiah Norris <jjno91@gmail.com>
2022-07-11 16:17:36 -0500

    add counts and main.tf

--- cluster.tf
+++ cluster.tf
@@ -1,9 +1,11 @@
 resource "aws_ecs_cluster" "this" {
-  name = var.name
+  count = 1
+  name  = var.name
 }
 
 resource "aws_ecs_cluster_capacity_providers" "this" {
-  cluster_name       = aws_ecs_cluster.this.name
+  count              = 1
+  cluster_name       = aws_ecs_cluster.this[0].name
   capacity_providers = ["FARGATE"]
 
   default_capacity_provider_strategy {
