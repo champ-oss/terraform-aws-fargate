592700779b66a49755b8ac417c88be41b134b05d
Jeremiah Norris <jjno91@gmail.com>
2022-07-12 15:58:13 -0500

    add security group

---
+++ security.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "this" {
+  name   = local.id
+  vpc_id = local.vpc_id
+  tags   = local.tags
+}
+
+resource "aws_security_group_rule" "egress" {
+  description       = "internet"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "all"
+  security_group_id = aws_security_group.this.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "ingress" {
+  description              = "alb"
+  type                     = "ingress"
+  from_port                = var.port
+  to_port                  = var.port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.this.id
+  source_security_group_id = data.aws_security_group.this.id
+}
