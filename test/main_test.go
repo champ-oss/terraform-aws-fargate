7a2b87bf924f557e10818c0f883b5a2629d70ec3
Jeremiah Norris <jjno91@gmail.com>
2022-07-25 15:58:58 -0500

    remove debugging tests

--- test/main_test.go
+++ test/main_test.go
@@ -1,27 +1,15 @@
-package test
+package this
 
 import (
-	"fmt"
 	"testing"
-	"time"
 )
-
-func init() {
-	fmt.Println("start init")
-	time.Sleep(8 * time.Second)
-	fmt.Println("end init")
-}
 
 func TestOne(t *testing.T) {
 	t.Parallel()
-
-	fmt.Println("in test 1")
 	t.Log("in test 1")
 }
 
 func TestTwo(t *testing.T) {
 	t.Parallel()
-
-	fmt.Println("in test 2")
 	t.Log("in test 2!")
 }
